9e20d720f79ebbc5c578954d27cf2b381928a4db
Тимур Калимуллин <kalimullin.tt@phystech.edu>
2024-03-30 12:29:03 +0300

    Initial Commit

---
+++ vk-internship-yula/vk_internship_yulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vk_internship_yulaApp.swift
+//  vk-internship-yula
+//
+//  Created by Тимур Калимуллин on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct vk_internship_yulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
